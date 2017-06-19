bd26728339ea03571d8fa33f14a18d242151902b
李磊 <1198189741@qq.com>
2017-06-20 00:22:08 +0800

    新增HelloWorld文件

--- src/com/lilei1998/MultiFor.java
+++ src/com/lilei1998/MultiFor.java
@@ -1,10 +1,10 @@
-package com.shrbank;
+package com.lilei1998;
 
 /**
  * Created by user on 2017/4/12.
  */
-public class MultiFor {
 
+public class MultiFor {
     public static void main(String[] args) {
         for (int x = 0; x < 4; x++) {
                 for (int y = 4; y > 2; y--) {
@@ -13,8 +13,6 @@ public class MultiFor {
             if (x == 1) {
                 x++;
             }
-
         }
-
     }
 }
