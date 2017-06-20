89c52b54e583c4fa6f077fa9c5df36fecff5f78a
李磊 <1198189741@qq.com>
2017-06-20 23:26:31 +0800

    新增environment

---
+++ src/com/lilei1998/Environment/Welcome.java
@@ -0,0 +1,15 @@
+/**
+  *@version 2017-06-20
+  *@author lilei
+  */
+
+public class Welcome {
+	public static void main(String[] args) {
+		String greeting = "Welcome to core Java!";
+		System.out.println(greeting);
+		for (int i = 0; i < greeting.lentgh(); i++) {
+			System.out.print("=");
+		System.out.println();
+		}
+	}
+}
