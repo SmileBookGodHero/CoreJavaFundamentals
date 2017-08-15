2f72c103aa9cf8eaac6ee84808ae68582aa76a0a
lilei11981 <1198189741@qq.com>
2017-08-15 09:19:24 +0800

    新增ConstructorTest类、修改配置文件

---
+++ src/com/lilei1998/oop/ConstructorTest.java
@@ -0,0 +1,24 @@
+package com.lilei1998.oop;
+
+import sun.rmi.runtime.Log;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by lilei on 2017/8/14 下午4:41.
+ *
+ * 重载构造器
+ * 用 this(...) 调用另一个构造器
+ * 无参数构造器
+ * 对象初始化块
+ * 静态初始化块
+ * 实例域初始化
+ */
+
+public class ConstructorTest {
+
+
+}
