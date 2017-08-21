bfb8ccdf5e5e5ca2c0ea03fc84202702fbc5b745
lilei <lilei@shrbank.com>
2017-08-21 16:22:11 +0800

    新增ConstructorTest构造器类

--- src/com/lilei1998/oop/ConstructorTest.java
+++ src/com/lilei1998/oop/ConstructorTest.java
@@ -1,11 +1,6 @@
 package com.lilei1998.oop;
 
-import sun.rmi.runtime.Log;
-
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.Random;
 
 /**
  * Created by lilei on 2017/8/14 下午4:41.
@@ -19,6 +14,62 @@ import java.util.Set;
  */
 
 public class ConstructorTest {
+    public static void main(String[] args) {
+        EmployeeExample[] staff = new EmployeeExample[3];
 
+        staff[0] = new EmployeeExample("Harry",40000);
+        staff[1] = new EmployeeExample(60000);
+        staff[2] = new EmployeeExample();
 
+        for (EmployeeExample employeeExample : staff) {
+            System.out.println("name = " + employeeExample.getName()
+            + ", id = " + employeeExample.getId()
+            + ", salary = " + employeeExample.getSalary());
+        }
+    }
+}
+
+class EmployeeExample {
+    private static int nextId;
+
+    private int id;
+    private String name = "";
+    private double salary;
+
+    static {    // 随机数生成器
+        Random generator = new Random();
+        nextId = generator.nextInt(10000);
+    }
+
+    {
+        id = nextId;
+        nextId++;
+    }
+
+    public EmployeeExample(String n, double s) {
+        name = n;
+        salary = s;
+    }
+
+    public EmployeeExample(double s) {
+        this("Employee #" + nextId, s);
+    }
+
+    public EmployeeExample() {   // 默认构造器
+        // name 初始化为 ""
+        // salary 初始化为 0
+        // id 初始化为 初始化块
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
