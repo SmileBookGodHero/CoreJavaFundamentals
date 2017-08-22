b527628b44ab940c4bef62450f8785e794c95e3b
lilei11981 <1198189741@qq.com>
2017-08-22 18:57:13 +0800

    新增EqualsTest类

---
+++ src/com/lilei1998/inheritance/equals/Manager.java
@@ -0,0 +1,43 @@
+package com.lilei1998.inheritance.equals;
+
+/**
+ * Created by lilei on 2017/8/22 下午3:57.
+ * 继承 Employee 类
+ */
+public class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    @Override
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (!super.equals(otherObject)) {
+            return false;
+        }
+        Manager other = (Manager) otherObject;
+        return bonus == other.bonus;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() + 17 * new Double(bonus).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "[bonus = " + bonus + "]";
+    }
+}
