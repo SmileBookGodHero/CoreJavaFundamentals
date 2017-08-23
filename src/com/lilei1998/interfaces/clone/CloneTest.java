716e3696feb591d605f8ae1984dbfc108e3f18c3
lilei11981 <1198189741@qq.com>
2017-08-23 17:35:15 +0800

    新增CloneTest类

---
+++ src/com/lilei1998/interfaces/clone/CloneTest.java
@@ -0,0 +1,22 @@
+package com.lilei1998.interfaces.clone;
+
+/**
+ * Created by lilei on 2017/8/23 下午5:13.
+ * 克隆的使用
+ */
+public class CloneTest {
+    public static void main(String[] args) {
+        try {
+            Employee original = new Employee("John Q. Public", 50000);
+            original.setHireDay(2000, 1,1);
+            Employee copy = original.clone();
+            copy.raiseSalary(10);
+            copy.setHireDay(2002,12,31);
+            System.out.println("original = " + original);
+            System.out.println("copy = " + copy);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/com/lilei1998/interfaces/clone/Employee.java
@@ -0,0 +1,41 @@
+package com.lilei1998.interfaces.clone;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by lilei on 2017/8/23 下午5:16.
+ */
+public class Employee implements Cloneable {
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        hireDay = new Date();
+    }
+
+    @Override
+    public Employee clone() throws CloneNotSupportedException {
+        Employee cloned = (Employee) super.clone();
+        cloned.hireDay = (Date) hireDay.clone();
+        return cloned;
+    }
+
+    public void setHireDay(int year, int month, int day) {
+        Date newHireDay = new GregorianCalendar(year, month - 1, day).getTime();
+        hireDay.setTime(newHireDay.getTime());
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[name = " + name + ", salary = " + salary + ", hireDay = " + hireDay + "]";
+    }
+}
