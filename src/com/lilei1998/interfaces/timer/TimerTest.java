084c8597a0783bddf064670b8025ad052d7853f3
lilei11981 <1198189741@qq.com>
2017-08-23 16:47:30 +0800

    参数修改

--- src/com/lilei1998/interfaces/timer/TimerTest.java
+++ src/com/lilei1998/interfaces/timer/TimerTest.java
@@ -24,7 +24,7 @@ public class TimerTest {
     public static void main(String[] args) {
         ActionListener listener = new TimePrinter();
 
-        Timer timer = new Timer(10000, listener);
+        Timer timer = new Timer(1000, listener);
         timer.start();
 
         JOptionPane.showMessageDialog(null, "Quit program?");
