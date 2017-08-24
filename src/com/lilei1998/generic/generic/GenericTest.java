efd1d18a5c70d00faf95d00749fcb911425d88c0
lilei <lilei@shrbank.com>
2017-08-24 20:21:33 +0800

    泛型示例

--- src/com/lilei1998/generic/generic/GenericTest.java
+++ src/com/lilei1998/generic/generic/GenericTest.java
@@ -9,14 +9,24 @@ import java.util.List;
 public class GenericTest {
 
     public static void main(String[] args) {
-        List list = new ArrayList();
-        list.add("sansa");
-        list.add("arya");
-        list.add(100);
+//        List list = new ArrayList();
+//        list.add("sansa");
+//        list.add("arya");
+//        list.add(100);
+//        for (int i = 0; i < list.size(); i++) {
+//            String name = (String) list.get(i); // 出现 ClassCastException 异常
+//            System.out.println("name: " + name);
+//        }
 
+        List<String> list = new ArrayList<>();  // 采用泛型的写法
+        list.add("Sansa");
+        list.add("Arya");
+//        list.add(100);   //  不能添加 Integer 类型
         for (int i = 0; i < list.size(); i++) {
-            String name = (String) list.get(i); // 1
-            System.out.println("name:" + name);
+            String name = list.get(i);
+            System.out.println("name: " + name);
         }
     }
 }
+
+
