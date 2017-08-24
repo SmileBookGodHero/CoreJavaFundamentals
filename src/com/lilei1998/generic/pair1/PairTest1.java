22a55a1ebe5f045b50f0b143cbdbf2f16f81fd1e
lilei <lilei@shrbank.com>
2017-08-24 18:48:21 +0800

    新增PairTest1类和GenericTest类

---
+++ src/com/lilei1998/generic/pair1/PairTest1.java
@@ -0,0 +1,72 @@
+package com.lilei1998.generic.pair1;
+
+
+//import javafx.util.Pair;
+
+//import com.sun.tools.javac.util.Pair;
+
+/**
+ * Created by lilei on 2017/8/24 下午5:52.
+ */
+public class PairTest1 {
+//    public static void main(String[] args) {
+//        String[] words = {"Mary", "had", "a", "little", "lamb"};
+//        Pair<String> mm = ArrayAlg.minmax(words);
+//        System.out.println("min = " + mm.getFirst());
+//        System.out.println("max = " + mm.getSecond());
+//    }
+}
+
+//class ArrayAlg {
+//    public static Pair<String> minmax(String[] a) {
+//        if (a == null || a.length == 0) {
+//            return null;
+//        }
+//
+//        String min = a[0];
+//        String max = a[0];
+//
+//        for (int i = 1; i < a.length; i++) {
+//            if (min.compareTo(a[i] > 0)) {
+//                min = a[i];
+//            }
+//
+//            if (max.compareTo(a[i] < 0)) {
+//                max = a[i];
+//            }
+//        }
+//
+//        return new Pair<>(min, max);
+//    }
+//}
+
+//class Pair<T,V> {
+//    private T first;
+//    private V second;
+//
+//    public Pair() {
+//        first = null;
+//        second = null;
+//    }
+//
+//    public Pair<T first, V second> {
+//        this.first = first;
+//        this.second = second;
+//    }
+//
+//    public T getFirst() {
+//        return first;
+//    }
+//
+//    public T getSecond() {
+//        return second;
+//    }
+//
+//    public void setFirst(T newValue) {
+//        first = newValue;
+//    }
+//
+//    public void setSecond(T newValue) {
+//        second = newValue;
+//    }
+//}
