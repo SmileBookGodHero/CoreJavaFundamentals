ad7cdeaf60d655d3b28ebe9de682555f3adc20d5
lilei11981 <1198189741@qq.com>
2017-08-25 09:54:54 +0800

    泛型示例

--- src/com/lilei1998/generic/generic/GenericTest.java
+++ src/com/lilei1998/generic/generic/GenericTest.java
@@ -1,32 +1,68 @@
 package com.lilei1998.generic.generic;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by lilei on 2017/8/24 下午6:45.
  */
 public class GenericTest {
 
-    public static void main(String[] args) {
-//        List list = new ArrayList();
-//        list.add("sansa");
-//        list.add("arya");
-//        list.add(100);
+//    public static void main(String[] args) {
+////        不使用泛型而使用强制类型转换
+////        List list = new ArrayList();
+////        list.add("sansa");
+////        list.add("arya");
+////        list.add(100);
+////        for (int i = 0; i < list.size(); i++) {
+////            String name = (String) list.get(i); // 出现 ClassCastException 异常
+////            System.out.println("name: " + name);
+////        }
+//
+//        // 采用泛型的写法
+//        List<String> list = new ArrayList<>();
+//        list.add("Sansa");
+//        list.add("Arya");
+////        list.add(100);   //  不能添加 Integer 类型
 //        for (int i = 0; i < list.size(); i++) {
-//            String name = (String) list.get(i); // 出现 ClassCastException 异常
+//            String name = list.get(i);
 //            System.out.println("name: " + name);
 //        }
+//    }
 
-        List<String> list = new ArrayList<>();  // 采用泛型的写法
-        list.add("Sansa");
-        list.add("Arya");
-//        list.add(100);   //  不能添加 Integer 类型
-        for (int i = 0; i < list.size(); i++) {
-            String name = list.get(i);
-            System.out.println("name: " + name);
-        }
+    public static void main(String[] args) {
+
+        Box<String> name = new Box<String>("Jon");
+        Box<Integer> age = new Box<Integer>(520);
+        Box<Number> number = new Box<Number>(1314);
+
+        getData(name);
+        getData(age);
+        getData(number);
     }
+
+    public static void getData(Box<?> data) {    // 使用类型通配符
+        System.out.println("data :" + data.getData());
+    }
+}
+
+class Box<T> {
+
+    private T data;
+
+    public Box() {
+
+    }
+
+    public Box(T data) {
+        setData(data);
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
 }
 
 
