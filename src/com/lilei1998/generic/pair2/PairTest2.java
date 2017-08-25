42c41e621a89c6f376dd9607405734c4342ae37d
lilei11981 <1198189741@qq.com>
2017-08-25 11:38:44 +0800

    新增PairTest2类

---
+++ src/com/lilei1998/generic/pair2/PairTest2.java
@@ -0,0 +1,43 @@
+package com.lilei1998.generic.pair2;
+
+import com.lilei1998.generic.pair1.Pair;
+
+import java.time.LocalDate;
+
+/**
+ * Created by lilei on 2017/8/25 上午11:27.
+ * 泛型：类型变量的限定
+ */
+public class PairTest2 {
+    public static void main(String[] args) {
+        LocalDate[] birthdays = {
+                LocalDate.of(1906, 12, 9),  // G. Hopper
+                LocalDate.of(1815, 12, 10), // A. Lovelace
+                LocalDate.of(1903, 12, 3), // J. von Neumann
+                LocalDate.of(1910, 6, 22), // K. Zuse
+        };
+
+        Pair<LocalDate> mm = ArrayAlg.minmax(birthdays);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+    }
+}
+
+class ArrayAlg {
+    public static <T extends Comparable> Pair<T> minmax(T[] a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+        T min = a[0];
+        T max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0) {
+                min = a[i];
+            }
+            if (max.compareTo(a[i]) < 0) {
+                max = a[i];
+            }
+        }
+        return new Pair<>(min, max);
+    }
+}
