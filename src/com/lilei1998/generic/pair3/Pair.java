4ca06ed3aa0a35aed3fe45580cc1f8ef4107d239
lilei11981 <1198189741@qq.com>
2017-08-26 14:19:21 +0800

    新增PairTest3类

---
+++ src/com/lilei1998/generic/pair3/Pair.java
@@ -0,0 +1,36 @@
+package com.lilei1998.generic.pair3;
+
+/**
+ * Created by lilei on 2017/8/25 上午10:14.
+ */
+
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() {
+        first = null;
+        second = null;
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setFirst(T newValue) {
+        first = newValue;
+    }
+
+    public void setSecond(T newValue) {
+        second = newValue;
+    }
+}
