81ee2571d5335755dfa2189f95e1318458fb2a5c
lilei11981 <1198189741@qq.com>
2017-08-29 09:03:02 +0800

    增加泛型注释

--- src/com/lilei1998/generic/pair2/PairTest2.java
+++ src/com/lilei1998/generic/pair2/PairTest2.java
@@ -42,3 +42,21 @@ class ArrayAlg {
         return new Pair<>(min, max);
     }
 }
+
+
+/**
+ *  Java 泛型转换：
+ *  1. 虚拟机中没有泛型，只有普通的类和方法
+ *  2. 所有的类型参数都用它们的限定类型替换
+ *  3. 桥方法被合成来保持多态
+ *  4. 为保持类型安全性，必要时插入强制类型转换
+ *
+ *  约束与局限性
+ *  1. 不能用基本类型实例化类型参数，没有 Pair<double>， 只有 Pair<Double>
+ *  2. 运行时类型查询只适用于原始类型，语句 if(a instanceof Pair<T>)  // Error
+ *  3. 不能创建参数化类型数组，语句Pair<String> table = new Pair<String>[10];  // Error
+ *  4. Varargs 警告
+ *  5. 不能实例化类型变量
+ *  6. 不能构造泛型数组
+ *  7. 泛型
+ */
