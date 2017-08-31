acb12fee6d473b5e25cc2ccc99fa4f3c1eec39b3
lilei <lilei@shrbank.com>
2017-08-31 18:01:48 +0800

    新增ShuffleTest类

---
+++ src/com/lilei1998/collection/shuffle/ShuffleTest.java
@@ -0,0 +1,22 @@
+package com.lilei1998.collection.shuffle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by lilei on 2017/8/31 下午5:51.
+ */
+public class ShuffleTest {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 49; i++) {
+            numbers.add(i);
+        }
+
+        Collections.shuffle(numbers);
+        List<Integer> winningCombination = numbers.subList(0, 6);
+        Collections.sort(winningCombination);
+        System.out.println(winningCombination);
+    }
+}
