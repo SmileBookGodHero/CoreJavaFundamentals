0ef050c466f610c00f3f58b6b64850028e769a42
lilei <lilei@shrbank.com>
2017-08-31 17:49:31 +0800

    修改MapTest类，新增Employee类

---
+++ src/com/lilei1998/collection/map/Employee.java
@@ -0,0 +1,14 @@
+package com.lilei1998.collection.map;
+
+/**
+ * Created by lilei on 2017/8/31 下午5:34.
+ */
+public class Employee {
+    private String name;
+    public Employee(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
