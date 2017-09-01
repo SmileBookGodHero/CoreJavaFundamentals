714c4e2b674a6fda50f97f4cb3d26d2270d6f4bb
lilei <lilei@shrbank.com>
2017-09-01 10:22:33 +0800

    新增InputTest类

---
+++ src/com/lilei1998/basicCode/InputTest.java
@@ -0,0 +1,26 @@
+package com.lilei1998.basicCode;
+
+import java.util.Scanner;
+
+/**
+ * Created by lilei on 2017/9/1 上午10:08.
+ */
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        System.out.print("How old are you? ");
+        int age = in.nextInt();
+
+        System.out.println("Hello, " + name + ". Next year you'll be " + (age + 1));
+
+//        Console console = System.console();
+//        String userName = console.readLine("User name: ");
+//        char[] password = console.readPassword("Password: ");
+//
+//        System.out.println("User name: " + userName + " Password: " + password);
+    }
+}
