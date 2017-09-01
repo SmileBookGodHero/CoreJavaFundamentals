67245c8089f0623ca3ef8286fc15d2e7169c61a3
lilei <lilei@shrbank.com>
2017-09-01 08:53:57 +0800

    新增Sieve类

---
+++ src/com/lilei1998/collection/sieve/Sieve.java
@@ -0,0 +1,40 @@
+package com.lilei1998.collection.sieve;
+
+import java.util.BitSet;
+
+/**
+ * Created by lilei on 2017/8/31 下午6:03.
+ */
+public class Sieve {
+    public static void main(String[] s) {
+        int n = 2000000;
+        long start = System.currentTimeMillis();
+        BitSet bitSet = new BitSet(n + 1);
+        int count = 0;
+        int i;
+        for (i = 0; i <= n; i++) {
+            bitSet.set(i);
+        }
+        i = 2;
+        while (i * i <= n) {
+            if (bitSet.get(i)) {
+                count++;
+                int k = 2 * i;
+                while (k <= n) {
+                    bitSet.clear(k);
+                    k += i;
+                }
+            }
+            i++;
+        }
+        while (i <= n) {
+            if (bitSet.get(i)) {
+                count++;
+            }
+            i++;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(count + " primes");
+        System.out.println((end - start) + " milliseconds");
+    }
+}
